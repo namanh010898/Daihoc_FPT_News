edba26ecec61cadced746f1ac04282bb557e56dd
namanh-nguyen <80470978+namanhnguyen@users.noreply.github.com>
2022-02-09 23:13:49 +0700

    Thêm footer header vào layout, thêm trang 404

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Daihoc_FPT_News.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
