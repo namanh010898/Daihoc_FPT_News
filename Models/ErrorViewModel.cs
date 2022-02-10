9e239cd316ec2979faecf0f8d952e72575aefce2
namanh-nguyen <80470978+namanhnguyen@users.noreply.github.com>
2022-02-10 18:22:06 +0700

    Sửa .dotnet 6.0 xuống 5.0

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Daihoc_FPT_News.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
